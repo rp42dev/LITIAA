416c59c6e9fdafdf1532eb5c7b1d44b106694936
Raivis Petrovskis <rapet80@gmail.com>
2023-05-29 10:27:29 +0100

    add nav style

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
 
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shrink');
+        } else if (window.scrollY > 100) {
+            navbarCollapsible.classList.add('navbar-shrink');
         }
+    };
 
+    // Collapse the navbar when page is scrolled
+    let prevScrollPos = window.pageYOffset;
+    let navbarCollapse = document.body.querySelector('.navbar-collapse');
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+        navbarCollapse.classList.remove('show');
+        if (prevScrollPos > currentScrollPos) {
+            document.body.querySelector('#mainNav').style.top = "0";
+        } else {
+            document.body.querySelector('#mainNav').style.top = "-100%";
+        }
+        prevScrollPos = currentScrollPos;
     };
 
     // Shrink the navbar 
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    window.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
@@ -28,7 +37,5 @@ window.addEventListener('DOMContentLoaded', event => {
             target: '#mainNav',
             rootMargin: '0px 0px -40%',
         });
-    };
-
-
+    }
 });
